9fcd18a57feee11eee88b8b01d6c14f0466c8ae4
Alex Heil <aheil4250@aol.com>
2015-12-05 14:04:09 -0500

    minor tweaks

---
+++ app/helpers/profiles_helper.rb
@@ -0,0 +1,29 @@
+module ProfilesHelper
+
+  def music_genres
+    [
+      ['Alternative'],
+      ['Ambient'],
+      ['Blues'],
+      ['Christian & Gospel'],
+      ['Classical'],
+      ['Comedy'],
+      ['Country'],
+      ['Electronic'],
+      ['Experimental'],
+      ['Hip-Hop/Rap'],
+      ['Indie'],
+      ['Jazz'],
+      ['Latino'],
+      ['Metal'],
+      ['Pop'],
+      ['R&B/Soul'],
+      ['Rock'],
+      ['Singer/Songwriter'],
+      ['Soundtrack'],
+      ['World']
+    ]
+  end
+
+end
+
