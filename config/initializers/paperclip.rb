338f89adaf6bbb02445e63edbe0630d1f36362d1
Alex Heil <aheil4250@aol.com>
2016-02-15 21:41:14 -0500

    paperclip fail probably

--- config/initializers/paperclip.rb
+++ config/initializers/paperclip.rb
@@ -1,3 +1,3 @@
 Paperclip.interpolates(:s3_us_west_url) { |attachment, style|
-  "#{attachment.s3_protocol}://s3-us-west-1.amazonaws.com/#{attachment.bucket_name}/#{attachment.path(style).gsub(%r{^/}, '')}"
+  "#{attachment.s3_protocol}://s3-us-west-2.amazonaws.com/#{attachment.bucket_name}/#{attachment.path(style).gsub(%r{^/}, '')}"
 }
