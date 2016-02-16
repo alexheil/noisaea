b0c27157f02f6ac65b69c3b2e16616931133df0b
Alex Heil <aheil4250@aol.com>
2016-02-15 21:20:31 -0500

    paperclip fail probably

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -92,7 +92,7 @@ Rails.application.configure do
 
   config.paperclip_defaults = {
     storage: :s3,
-    s3_region: ENV['MY_AWS_REGION'],
+    s3_region: 'us-west-2',
     s3_credentials: {
       bucket: ENV['MY_S3_BUCKET_NAME'],
       access_key_id: ENV['MY_AWS_ACCESS_KEY_ID'],
