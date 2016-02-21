b92a4ce215658152bbb1b903ad3c0b7c1e7eb90b
Alex Heil <aheil4250@aol.com>
2016-02-20 19:12:23 -0500

    scheduler

--- app/models/artist_notification.rb
+++ app/models/artist_notification.rb
@@ -15,7 +15,7 @@ class ArtistNotification < ActiveRecord::Base
   validates :notice_type, presence: true
 
   def self.notification_cleanup
-    ArtistNotification.where(read: true).where("updated_at > ?", 3.days.ago).find_each do |notification|
+    ArtistNotification.where(read: true).where("updated_at < ?", 3.days.ago).find_each do |notification|
       notification.destroy
     end
   end
