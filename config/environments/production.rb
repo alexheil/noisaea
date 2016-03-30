c8ccaf8f012581ed789d8c2eb4484ffeba4c1add
Alex Heil <aheil4250@aol.com>
2016-03-30 19:12:23 -0400

    stripe

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -94,6 +94,7 @@ Rails.application.configure do
     :storage => :s3,
     :s3_region => 'us-west-2',
     :s3_host_name => 's3-us-west-2.amazonaws.com',
+    :s3_protocol => :https,
     :s3_credentials => {
       :bucket => ENV['S3_BUCKET_NAME'],
       :access_key_id => ENV['AWS_ACCESS_KEY_ID'],
