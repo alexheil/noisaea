01ff605bcf3a9a4840ec02b19c4ddb829792c8dd
Alex Heil <aheil4250@aol.com>
2016-03-31 23:22:02 -0400

    stripe

--- app/controllers/artists/payment_settings_controller.rb
+++ app/controllers/artists/payment_settings_controller.rb
@@ -36,8 +36,8 @@ class Artists::PaymentSettingsController < ApplicationController
           currency: @account.default_currency,
           country: @account.country,
           stripe_id: @account.id,
-          stripe_publishable_key: @account.keys.publishable,
-          stripe_secret_key: @account.keys.secret
+          stripe_publishable_key: @account[:keys][:publishable],
+          stripe_secret_key: @account[:keys][:secret]
         )
     end
 
