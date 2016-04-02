45379abdede69b674014441f4a39bc39e08508b5
Alex Heil <aheil4250@aol.com>
2016-04-01 20:26:05 -0400

    stripe

--- app/controllers/artists/payment_settings_controller.rb
+++ app/controllers/artists/payment_settings_controller.rb
@@ -44,7 +44,7 @@ class Artists::PaymentSettingsController < ApplicationController
         )
     end
 
-    redirect_to edit_artist_payment_settings_path(@artist)
+    redirect_to artist_path(@artist)
   end
 
   def edit
