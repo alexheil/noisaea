b52413e1db1ed6288a7719dd0667766bd2274cd1
Alex Heil <aheil4250@aol.com>
2016-05-03 11:16:24 -0400

    direct upload for tracks

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -1,7 +1,5 @@
 //= require jquery
 //= require jquery_ujs
-//= require jquery-fileupload/vendor/jquery.ui.widget
-//= require jquery-fileupload/jquery.fileupload
 //= require_tree .
 
 var micropostComments = function() {
