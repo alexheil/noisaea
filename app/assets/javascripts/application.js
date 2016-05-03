ad337c7fb7f913893d6b8a2972f77283712ef320
Alex Heil <aheil4250@aol.com>
2016-05-03 12:52:39 -0400

    direct upload for tracks

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -674,3 +674,18 @@ var lyricsAndTrack = function() {
 
 $(document).ready(lyricsAndTrack);
 $(document).on('page:load', lyricsAndTrack);
+
+var fileTrackUpload = function() {
+
+    $('.directUpload').find("input:file").each(function(i, elem) {
+      var fileInput    = $(elem);
+      var form         = $(fileInput.parents('form:first'));
+      var submitButton = form.find('input[type="submit"]');
+      var progressBar  = $("<div class='bar'></div>");
+      var barContainer = $("<div class='progress'></div>").append(progressBar);
+      fileInput.after(barContainer);
+    });
+  };
+
+$(document).ready(fileTrackUpload);
+$(document).on('page:load', fileTrackUpload);
