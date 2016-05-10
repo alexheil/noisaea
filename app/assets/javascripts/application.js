9359b2aff87a3538e799170a7d829ce31efdeb85
Alex Heil <aheil4250@aol.com>
2016-05-09 23:25:06 -0400

    direct upload for tracks

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -3,66 +3,6 @@
 //= require jquery-fileupload/basic-plus
 //= require_tree .
 
-
-var directUpload = function() {
-
-  var fileInput = $('.musicInput');
-  var form = $(fileInput.parents('form:first'));
-  var submitButton = form.find('input[type="submit"]');
-  var uploadButton = $('.uploadButton');
-  var progressBar = $("<div class='bar'></div>");
-  var barContainer = $("<div class='progress'></div>").append(progressBar);
-  fileInput.after(barContainer);
-  fileInput.fileupload({
-    fileInput: fileInput,
-    url: form.data('url'),
-    type: 'POST',
-    autoUpload: false,
-    formData: form.data('form-data'),
-    paramName: 'file',
-    dataType: 'XML',
-    replaceFileInput: false,
-    add: function (e, data) {
-      fileInput.after(uploadButton);
-      uploadButton.click(function() {
-        data.submit();
-      });
-    },
-    progress: function (e, data) {
-      var progress = parseInt(data.loaded / data.total * 100, 10);
-      progressBar.css('width', progress + '%')
-    },
-    start: function (e) {
-      submitButton.prop('disabled', true);
-      progressBar.
-        css('background', 'green').
-        css('display', 'block').
-        css('width', '0%').
-        text("Loading...");
-    },
-    done: function(e, data) {
-      submitButton.prop('disabled', false);
-      progressBar.text("Upload finished.");
-      uploadButton.hide();
-      var key   = $(data.jqXHR.responseXML).find("Key").text();
-      var url   = form.data('url') + '/' + key;
-      var input = $("<input />", { type:'hidden', name: fileInput.attr('name'), value: url })
-      form.append(input);
-    },
-    fail: function(e, data) {
-      submitButton.prop('disabled', false);
-      progressBar.
-        css("background", "red").
-        text("Failed");
-    }
-  });
-
-};
-
-$(document).ready(directUpload);
-$(document).on('page:load', directUpload);
-
-
 var micropostComments = function() {
   $('.microposts_js').on("click", ".linden", function() {
     $(this).closest('.lisbon').next('.walden').slideToggle(300);
@@ -579,7 +519,6 @@ var merchLightbox = function() {
 $(document).ready(merchLightbox);
 $(document).on('page:load', merchLightbox);
 
-
 var musicPlayer = function() {
 
   var music = document.getElementById('audioplayer');
@@ -691,7 +630,6 @@ $(document).ready(musicPlayer);
 $(document).on('page:load', musicPlayer);
 
 
-
 var lyricsAndTrack = function() {
 
   $('.artist-track-title').click(function() {
@@ -737,3 +675,62 @@ var lyricsAndTrack = function() {
 
 $(document).ready(lyricsAndTrack);
 $(document).on('page:load', lyricsAndTrack);
+
+
+var directUpload = function() {
+
+  var fileInput = $('.musicInput');
+  var form = $(fileInput.parents('form:first'));
+  var submitButton = form.find('input[type="submit"]');
+  var uploadButton = $('.uploadButton');
+  var progressBar = $("<div class='bar'></div>");
+  var barContainer = $("<div class='progress'></div>").append(progressBar);
+  fileInput.after(barContainer);
+  fileInput.fileupload({
+    fileInput: fileInput,
+    url: form.data('url'),
+    type: 'POST',
+    autoUpload: false,
+    formData: form.data('form-data'),
+    paramName: 'file',
+    dataType: 'XML',
+    replaceFileInput: false,
+    add: function (e, data) {
+      fileInput.after(uploadButton);
+      uploadButton.click(function() {
+        data.submit();
+      });
+    },
+    progress: function (e, data) {
+      var progress = parseInt(data.loaded / data.total * 100, 10);
+      progressBar.css('width', progress + '%')
+    },
+    start: function (e) {
+      submitButton.prop('disabled', true);
+      progressBar.
+        css('background', 'green').
+        css('display', 'block').
+        css('width', '0%').
+        text("Loading...");
+    },
+    done: function(e, data) {
+      submitButton.prop('disabled', false);
+      progressBar.text("Upload finished.");
+      uploadButton.hide();
+      var key   = $(data.jqXHR.responseXML).find("Key").text();
+      var url   = form.data('url') + '/' + key;
+      var input = $("<input />", { type:'hidden', name: fileInput.attr('name'), value: url })
+      form.append(input);
+    },
+    fail: function(e, data) {
+      submitButton.prop('disabled', false);
+      progressBar.
+        css("background", "red").
+        text("Failed");
+    }
+  });
+
+};
+
+$(document).ready(directUpload);
+$(document).on('page:load', directUpload);
